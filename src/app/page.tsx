4e687e58646ab5d3d5474d714dfb541ebe1e6c90
Eduardo Muchak <eduardomuchak@gmail.com>
2023-06-13 10:17:22 -0300

    build: :wrench: add eslint and prettier configurations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="w-screen h-screen flex items-center justify-center">
+    <div className="flex h-screen w-screen items-center justify-center">
       <h1 className="text-2xl font-bold">Hello World</h1>
     </div>
   );
