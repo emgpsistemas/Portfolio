05e5cfaebbaa84927ef7684c66a6fbb07adaa18d
Eduardo Muchak <eduardomuchak@gmail.com>
2023-06-13 13:57:39 -0300

    feat: :sparkles: creates all pages and adds all project assets

---
+++ src/app/servicos/page.tsx
@@ -0,0 +1,7 @@
+export default function Contato() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <h1 className="text-2xl font-bold">Serviços</h1>
+    </div>
+  );
+}
---
+++ src/app/sobre/page.tsx
@@ -0,0 +1,7 @@
+export default function Contato() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <h1 className="text-2xl font-bold">Sobre Nós</h1>
+    </div>
+  );
+}
