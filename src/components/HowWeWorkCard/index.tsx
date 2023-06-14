4750f99953296e664c773fbd3eea8620cdf39117
Eduardo Muchak <eduardomuchak@gmail.com>
2023-06-14 19:22:25 -0300

    fix: :art: adjusts the mobile view of how we work card component

--- src/components/HowWeWorkCard/index.tsx
+++ src/components/HowWeWorkCard/index.tsx
@@ -5,6 +5,11 @@ function HowWeWordCard({ id, title, description }: HowWeWork) {
   if (isMobile) {
     return (
       <div className="flex flex-col">
+        <div className="flex flex-1 items-start justify-center">
+          <h1 className="text-7xl font-bold leading-relaxed text-primary-blue-500">
+            {id}.
+          </h1>
+        </div>
         <div className="flex flex-col space-y-2">
           <h1 className="text-center text-[22px] font-bold leading-relaxed lg:text-2xl">
             {title}
