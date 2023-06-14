c7923c67abd467908a7acc74d655f81a954e85a2
Eduardo Muchak <eduardomuchak@gmail.com>
2023-06-14 10:23:44 -0300

    feat: :sparkles: finishes home page web version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,17 @@
 
 export default function Home() {
   return (
-    <>
-      <h1>EMGP Sistemas</h1>
-    </>
+    <div className="grid h-screen grid-flow-row grid-cols-2 items-center">
+      <div className="flex justify-end">
+        <div className="flex flex-col">
+          <h1 className="text-[76px] font-bold leading-relaxed">
+            EMGP Sistemas
+          </h1>
+          <h2 className="text-[44px] font-normal leading-none">
+            Web Fullstack & Mobile Development
+          </h2>
+        </div>
+      </div>
+    </div>
   );
 }
