acec9bd432896ff30d799a69977aeb99e1561678
Eduardo Muchak <eduardomuchak@gmail.com>
2023-06-15 16:14:27 -0300

    fix: :bug: remove next js api folder

--- src/app/api/contato/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export function POST(req: NextApiRequest, res: NextApiResponse) {
-  const { name, email } = req.body;
-
-  res.status(200).json({ message: "Dados recebidos com sucesso!" });
-}
