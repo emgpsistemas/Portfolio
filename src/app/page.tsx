d7d7f91d6c6418f902c731a2a8d01dd79415bbf5
Eduardo Muchak <eduardomuchak@gmail.com>
2023-06-16 11:39:18 -0300

    feat: :sparkles: add custom eslint config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,18 @@
 "use client";
 
-import GoToServicesPage from "@/components/GotoServicePage";
-
 export default function Home() {
   return (
-    <>
-      <div className="grid h-screen grid-flow-row grid-cols-1 items-center lg:grid-cols-2">
-        <div className="flex justify-center lg:justify-end">
-          <div className="flex flex-col ">
-            <h1 className="text-[40px] font-bold leading-relaxed md:text-6xl lg:text-[76px]">
-              EMGP Sistemas
-            </h1>
-            <h2 className="text-[16px] font-normal leading-none md:text-3xl lg:text-[44px]">
-              Web Fullstack & Mobile Development
-            </h2>
-          </div>
+    <div className="grid h-screen grid-flow-row grid-cols-1 items-center lg:grid-cols-2">
+      <div className="flex justify-center lg:justify-end">
+        <div className="flex flex-col space-y-5">
+          <h1 className="text-[40px] font-bold leading-relaxed md:text-6xl lg:text-[76px]">
+            EMGP Systems
+          </h1>
+          <h2 className="text-[16px] font-normal leading-tight md:text-3xl lg:text-[44px]">
+            Web Fullstack & Mobile Development
+          </h2>
         </div>
       </div>
-      <GoToServicesPage />
-    </>
+    </div>
   );
 }
