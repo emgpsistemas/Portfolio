1a94254644af97dd67094c22cd0ffa6dc87c9734
Eduardo Muchak <eduardomuchak@gmail.com>
2023-06-21 14:22:28 -0300

    fix: :art: adjusts title alignment of home section

--- src/components/HomeSection/index.tsx
+++ src/components/HomeSection/index.tsx
@@ -2,18 +2,20 @@ import Scroll100VHIcon from "../Scroll100VHIcon";
 
 function HomeSection() {
   return (
-    <div
-      className="relative grid h-screen grid-flow-row grid-cols-1 items-center lg:grid-cols-2"
-      id="home"
-    >
-      <div className="flex justify-center lg:justify-end">
-        <div className="flex flex-col space-y-5">
-          <h1 className="text-[40px] font-bold leading-relaxed md:text-6xl lg:text-[76px]">
-            EMGP Sistemas
-          </h1>
-          <h2 className="text-[16px] font-normal leading-tight md:text-3xl lg:text-[44px]">
-            Desenvolvimento de Sites e Aplicativos
-          </h2>
+    <div className=" relative h-screen w-screen">
+      <div
+        className="relative flex h-screen w-full items-center justify-center lg:max-w-[60vw]"
+        id="home"
+      >
+        <div className="flex justify-center lg:justify-end">
+          <div className="flex flex-col space-y-2 lg:space-y-2">
+            <h1 className="text-[40px] font-bold leading-tight md:text-6xl lg:text-[76px]">
+              EMGP Sistemas
+            </h1>
+            <h2 className="text-[16px] font-normal leading-tight md:text-3xl lg:text-[40px]">
+              Desenvolvimento de Sites e Aplicativos
+            </h2>
+          </div>
         </div>
       </div>
       <Scroll100VHIcon />
